89f7f8cda5d0865d8df2d405c0c053a8415be390
Arul Geroge <arul.george@auspost.com.au>
2017-09-13 11:45:02 +1000

    test key value pair

--- sample-app/src/main/java/com/example/controller/KeyValueController.java
+++ sample-app/src/main/java/com/example/controller/KeyValueController.java
@@ -3,10 +3,12 @@ package com.example.controller;
 import com.example.model.KeyValue;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Map;
 
@@ -14,14 +16,19 @@ import java.util.Map;
 public class KeyValueController {
 
     @RequestMapping(value = "/keyvalue", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, method = RequestMethod.POST)
-    public String test(@Valid @ModelAttribute KeyValue keyValue) {
+    public String test(@Valid @RequestBody KeyValue keyValue, HttpServletRequest request) {
 
         System.out.println("/keyvalue api");
         Map<String, String> input = keyValue.getDataFields();
 
-        input.forEach((k, v) -> {
-            System.out.println("Key:" + k + "Value:" + v);
-        });
+        System.out.println("Key:" + keyValue.getKey());
+
+        if (input != null) {
+            input.forEach((k, v) -> {
+                System.out.println("Key:" + k + ",Value:" + v);
+            });
+        }
+
         return "success";
     }
 }
--- sample-app/src/main/java/com/example/model/KeyValue.java
+++ sample-app/src/main/java/com/example/model/KeyValue.java
@@ -1,10 +1,14 @@
 package com.example.model;
 
+import javax.validation.constraints.NotNull;
 import java.util.Map;
 
 public class KeyValue {
 
     private Map<String, String> dataFields;
+
+    @NotNull
+    private String key;
 
     public Map<String, String> getDataFields() {
         return dataFields;
@@ -12,5 +16,13 @@ public class KeyValue {
 
     public void setDataFields(Map<String, String> dataFields) {
         this.dataFields = dataFields;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
     }
 }
